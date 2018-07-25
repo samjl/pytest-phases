682735c6193b97f75d93c23f8affe268e5670471
Sam Lea <sam.lea@aviatnet.com>
2018-07-25 13:38:41 +1200

    Add pymongo setup requirement

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='samjlea@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["pytest>=3.0.0", "future", "decorator"],
+    install_requires=["pytest>=3.0.0", "future", "decorator", "pymongo"],
     # the following makes a plugin available to pytest
     entry_points={'pytest11': ['phases = pytest_phases.pytest_phases']},
     # custom PyPI classifier for pytest plugins
