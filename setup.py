7bccd167db5695abe938e319aa8f03641c891e19
Sam Lea <sam.lea@aviatnet.com>
2018-08-06 11:25:59 +1200

    Fix: set logging directory within jenkins workspace

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pytest-phases",
-    version='0.2.0',
+    version='0.2.1',
     author='Sam Lea',
     author_email='samjlea@gmail.com',
     packages=find_packages(),
