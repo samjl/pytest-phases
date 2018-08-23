e1ce11d5f6558ab468a7e53f9ef69698fa52b211
Sam Lea <sam.lea@aviatnet.com>
2018-08-24 09:47:14 +1200

    Session dash support, bump version and update install requirement

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="pytest-phases",
-    version='0.4.0',
+    version='0.5.0',
     author='Sam Lea',
     author_email='samjlea@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["pytest>=3.0.0", "future", "decorator", "pymongo"],
+    install_requires=["pytest>=3.7.2", "future", "decorator",
+                      "pymongo>=3.7.1"],
     # the following makes a plugin available to pytest
     entry_points={'pytest11': ['phases = pytest_phases.pytest_phases']},
     # custom PyPI classifier for pytest plugins
