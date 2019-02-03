26a61ada2a3c6f570727bbc9a31aa61fc82b7745
Sam Lea <sam.lea@aviatnet.com>
2019-02-04 09:41:28 +1300

    Add missing log_method import

--- pytest_phases/__init__.py
+++ pytest_phases/__init__.py
@@ -1,3 +1,3 @@
 from .loglevels import LogLevel as log
-from .loglevels import LibraryLogging
+from .loglevels import LibraryLogging, log_method
 from .verify import verify, WarningException, VerificationException
