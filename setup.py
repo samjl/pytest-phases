e649d57b7971536aa4d485d922da770b7739efae
Sam Lea <sam.lea@aviatnet.com>
2019-02-12 13:10:01 +1300

    Bump version to v0.13.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pytest-phases",
-    version='0.13.0',
+    version='0.13.1',
     author='Sam Lea',
     author_email='samjlea@gmail.com',
     packages=find_packages(),
